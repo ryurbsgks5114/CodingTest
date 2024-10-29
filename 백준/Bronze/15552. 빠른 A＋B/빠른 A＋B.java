f5d4f3481bef287e173779193592b5367caadae6
ryurbsgks5114 <165640275+ryurbsgks5114@users.noreply.github.com>
2024-10-29 19:16:10 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 948 ms, Memory: 300028 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(bufferedReader.readLine());
+
+        Main main = new Main();
+
+        for (int i = 0; i < T; i++) {
+
+            String[] input = bufferedReader.readLine().split(" ");
+            Integer A = Integer.valueOf(input[0]);
+            Integer B = Integer.valueOf(input[1]);
+
+            bufferedWriter.write(main.addTwoNumbers(A, B) + "\n");
+
+        }
+
+        bufferedWriter.flush();
+
+    }
+
+    private Integer addTwoNumbers (Integer A, Integer B) {
+        return A + B;
+    }
+}
