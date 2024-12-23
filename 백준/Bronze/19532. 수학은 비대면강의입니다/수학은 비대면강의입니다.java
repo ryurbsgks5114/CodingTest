ed9987927bc328c142cb68b2de84e56ce8180ba6
ryurbsgks5114 <165640275+ryurbsgks5114@users.noreply.github.com>
2024-12-23 14:19:43 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 148 ms, Memory: 16776 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] input = bufferedReader.readLine().split(" ");
+
+        int a = Integer.parseInt(input[0]);
+        int b = Integer.parseInt(input[1]);
+        int c = Integer.parseInt(input[2]);
+        int d = Integer.parseInt(input[3]);
+        int e = Integer.parseInt(input[4]);
+        int f = Integer.parseInt(input[5]);
+
+        for (int i = -999; i <= 999; i++) {
+            for (int j = -999; j <= 999; j++) {
+                if (a * i + b * j == c && d * i + e * j == f) {
+                    bufferedWriter.write(i + " " + j);
+                    bufferedWriter.flush();
+
+                    return;
+                }
+            }
+        }
+
+    }
+}
