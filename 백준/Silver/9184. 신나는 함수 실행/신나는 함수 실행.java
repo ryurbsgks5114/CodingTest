dbe5561400530581b59ba16ae24474235ddc1ce3
ryurbsgks5114 <165640275+ryurbsgks5114@users.noreply.github.com>
2025-03-02 14:19:41 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 300 ms, Memory: 27844 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -0,0 +1,54 @@
+import java.io.*;
+
+public class Main {
+
+    static int[][][] dp = new int[51][51][51];
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while (true) {
+
+            String[] input = bufferedReader.readLine().split(" ");
+            int a = Integer.parseInt(input[0]);
+            int b = Integer.parseInt(input[1]);
+            int c = Integer.parseInt(input[2]);
+
+            if (a == -1 && b == -1 && c == -1) {
+                break;
+            }
+
+            int result = w(a, b, c);
+
+            bufferedWriter.write("w(" + a + ", " + b + ", " + c + ") = " + result + "\n");
+
+        }
+
+        bufferedWriter.flush();
+
+    }
+
+    public static int w(int a, int b, int c) {
+        if (a <= 0 || b <= 0 || c <= 0) {
+            return 1;
+        }
+
+        if (a > 20 || b > 20 || c > 20) {
+            return w(20, 20, 20);
+        }
+
+        if (dp[a][b][c] != 0) {
+            return dp[a][b][c];
+        }
+
+        if (a < b && b < c) {
+            dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+        } else {
+            dp[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+        }
+
+        return dp[a][b][c];
+    }
+}
