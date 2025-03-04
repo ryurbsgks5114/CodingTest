22c4822ed215ac21e127776ce4343a7662e90892
ryurbsgks5114 <165640275+ryurbsgks5114@users.noreply.github.com>
2025-03-04 16:07:05 +0900

    [Silver III] Title: 파도반 수열, Time: 120 ms, Memory: 15880 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(bufferedReader.readLine());
+        long[] dp = new long[101];
+
+        dp[1] = dp[2] = dp[3] = 1;
+        dp[4] = dp[5] = 2;
+
+        for (int i = 6; i <= 100; i++) {
+            dp[i] = dp[i - 1] + dp[i - 5];
+        }
+
+        for (int i = 0; i < T; i++) {
+
+            int N = Integer.parseInt(bufferedReader.readLine());
+
+            bufferedWriter.write(dp[N] + "\n");
+        }
+
+        bufferedWriter.flush();
+
+    }
+}
