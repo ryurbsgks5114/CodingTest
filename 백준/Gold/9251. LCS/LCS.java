a028b916869ff2e05cf9bc523b092ea54197ae0a
ryurbsgks5114 <165640275+ryurbsgks5114@users.noreply.github.com>
2025-03-15 12:46:00 +0900

    [Gold V] Title: LCS, Time: 132 ms, Memory: 18664 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String a = bufferedReader.readLine();
+        String b = bufferedReader.readLine();
+
+        int n = a.length();
+        int m = b.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (a.charAt(i - 1) == b.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        bufferedWriter.write(String.valueOf(dp[n][m]));
+
+        bufferedWriter.flush();
+
+    }
+}
