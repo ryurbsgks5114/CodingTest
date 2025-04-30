127196408f3f4bb48fd19e23690870c40f48ab54
ryurbsgks5114 <165640275+ryurbsgks5114@users.noreply.github.com>
2025-04-30 14:56:14 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 964 ms, Memory: 96920 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+    static int[] order;
+    static int cnt = 1;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int n = Integer.parseInt(stringTokenizer.nextToken());
+        int m = Integer.parseInt(stringTokenizer.nextToken());
+        int r = Integer.parseInt(stringTokenizer.nextToken());
+
+        graph = new ArrayList[n + 1];
+        visited = new boolean[n + 1];
+        order = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int u = Integer.parseInt(stringTokenizer.nextToken());
+            int v = Integer.parseInt(stringTokenizer.nextToken());
+            
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        for (int i = 1; i <= n; i++) {
+            graph[i].sort(Collections.reverseOrder());
+        }
+
+        dfs(r);
+
+        for (int i = 1; i <= n; i++) {
+            bufferedWriter.write(order[i] + "\n");
+        }
+
+        bufferedWriter.flush();
+
+    }
+
+    static void dfs(int node) {
+        visited[node] = true;
+        order[node] = cnt++;
+
+        for (int el: graph[node]) {
+            if (!visited[el]) {
+                dfs(el);
+            }
+        }
+    }
+}
